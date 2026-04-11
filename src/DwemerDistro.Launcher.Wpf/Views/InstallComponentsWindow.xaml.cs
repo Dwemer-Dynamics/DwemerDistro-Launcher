39550228c73d00dfafe31c25b9549e39e7c3d55a
RANGROO <reecemeakings@gmail.com>
2026-04-11 11:57:18 -0700

    Release 2.5.6

--- src/DwemerDistro.Launcher.Wpf/Views/InstallComponentsWindow.xaml.cs
+++ src/DwemerDistro.Launcher.Wpf/Views/InstallComponentsWindow.xaml.cs
@@ -1,12 +1,27 @@
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DwemerDistro.Launcher.Wpf.Views;
 
 public partial class InstallComponentsWindow : Window
 {
+    private const string DefaultDescriptionText = "Hover over a component below to see its description.";
+
     public InstallComponentsWindow()
     {
         InitializeComponent();
     }
-}
 
+    private void ComponentButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        if (sender is Button { ToolTip: string description } && !string.IsNullOrWhiteSpace(description))
+        {
+            DescriptionTextBlock.Text = description;
+        }
+    }
+
+    private void ComponentButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        DescriptionTextBlock.Text = DefaultDescriptionText;
+    }
+}
